a55386477bcc7d96fc010a1ca26865b29163c43a
Neha Lal <rani259@hotmail.com>
2019-11-04 13:22:22 -0800

    added search route

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,6 +4,7 @@ import Container from "../components/Container";
 import SearchForm from "../components/SearchForm";
 import SearchResults from "../components/SearchResults";
 import Alert from "../components/Alert";
+import axios from 'axios'
 
 class Search extends Component {
   state = {
@@ -14,24 +15,25 @@ class Search extends Component {
   };
 
   // When the component mounts, get a list of all available base breeds and update this.state.breeds
-  componentDidMount() {
-    API.getBaseBreedsList()
-      .then(res => this.setState({ breeds: res.data.message }))
-      .catch(err => console.log(err));
-  }
+  // componentDidMount() {
+  //   API.getBaseBreedsList()
+  //     .then(res => this.setState({ breeds: res.data.message }))
+  //     .catch(err => console.log(err));
+  // }
 
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-  };
+  // handleInputChange = event => {
+  //   this.setState({ search: event.target.value });
+  // };
 
   handleFormSubmit = event => {
     event.preventDefault();
-    API.getDogsOfBreed(this.state.search)
+    axios.get("hhtp://localhost:3001/recipeSearch/"+this.state.search)
       .then(res => {
-        if (res.data.status === "error") {
+        if (res.status === "error") {
           throw new Error(res.data.message);
         }
-        this.setState({ results: res.data.message, error: "" });
+        console.log(res)
+        // this.setState({ results: res.data.message, error: "" });
       })
       .catch(err => this.setState({ error: err.message }));
   };
@@ -49,7 +51,6 @@ class Search extends Component {
           <SearchForm
             handleFormSubmit={this.handleFormSubmit}
             handleInputChange={this.handleInputChange}
-            breeds={this.state.breeds}
           />
           <SearchResults results={this.state.results} />
         </Container>
