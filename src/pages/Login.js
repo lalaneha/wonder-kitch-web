e77881dc373f4599bc0f8f3d9a44dc63a96e47a2
Alex F <farrisat1@gmail.com>
2019-11-05 11:06:24 -0800

    working login issues

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -31,13 +31,14 @@ class Login extends Component {
     axios.post("http://localhost:3000/login", {logemail: this.state.logemail,  logpassword: this.state.logpassword})
       .then(res => {
         console.log("look here", res.data)
+        localStorage.setItem("userID", res.data._id)
         if (res.status === "error") {
           throw new Error(res.data.message);
         }
         // Tell the UI we've authenticated.
         fakeAuth.isAuthenticated = true;
         // React redirect to /home route.
-        // this.props.history.push("/home");
+        this.props.history.push("/home");
       })
       .catch(err => this.setState({ error: err.message }));
     this.setState({
